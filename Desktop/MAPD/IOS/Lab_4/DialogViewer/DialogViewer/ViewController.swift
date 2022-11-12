e624fac756e3b0d609555f9e9f38856d69ed4753
Chetanpatel57 <40812586+Chetanpatel57@users.noreply.github.com>
2022-11-12 10:45:52 -0500

    Initial Commit

---
+++ Desktop/MAPD/IOS/Lab_4/DialogViewer/DialogViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DialogViewer
+//
+//  Created by Chetan Patel on 2022-11-12.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
